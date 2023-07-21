16701bbb138a876ab5ca9b6764fa51112b64b4bb
Praveen-Kumar-Jain <praveenjain60211@gmail.com>
2023-07-21 09:43:06 +0530

    feat: Adds Store using redux toolkit

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,18 +1,22 @@
-import { Provider as ReduxProvider } from "react-redux";
+// import { Provider as ReduxProvider } from "react-redux";
 
 import "./App.css";
 // import Header from "./components/Header";
-import ProductForm from "./pages/Product";
-import configureStore from "./redux/configureStore";
-const store = configureStore();
+// import ProductForm from "./pages/Product";
+// import configureStore from "./redux/configureStore";
+// const store = configureStore();
+// import { Counter } from "./components/Counter";
+import Pokemon from "./components/pokemon";
 
 function App() {
   return (
     <div className="App">
       {/* <Header/> */}
-      <ReduxProvider store={store}>
-        <ProductForm />
-      </ReduxProvider>
+      {/* <ReduxProvider store={store}> */}
+        {/* <ProductForm /> */}
+      {/* </ReduxProvider> */}
+      {/* <Counter/> */}
+      <Pokemon/>
     </div>
   );
 }
